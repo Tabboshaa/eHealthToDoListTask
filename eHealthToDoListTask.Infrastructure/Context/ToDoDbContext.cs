3a79d2cc8d3105595f2ff4798708fdc610ccce47
abdelaziz tabbosha <abdelaziz.tabosha@nt-me.com>
2024-03-20 12:43:11 +0200

    Create ToDoDbContext Class

---
+++ eHealthToDoListTask.Infrastructure/Context/ToDoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using eHealthToDoListTask.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHealthToDoListTask.Infrastructure.Context
+{
+    public class ToDoDbContext :DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
